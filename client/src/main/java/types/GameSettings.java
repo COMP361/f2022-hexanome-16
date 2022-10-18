aa9d94bc068a86e6e7051d6d45b5c0c01d39e8dd
Constantin Buruiana <constbur@gmail.com>
2022-10-18 11:16:10 -0400

    added reusable types folder

---
+++ client/src/main/java/types/GameSettings.java
@@ -0,0 +1,8 @@
+package types;
+
+public class GameSettings {
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final String TITLE = "Game";
+    public static final int FPS = 60;
+}
