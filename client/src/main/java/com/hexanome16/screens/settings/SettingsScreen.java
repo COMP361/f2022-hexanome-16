81deec26281c0cc31e4b3da9a26ec1cf200c9ff8
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2022-10-28 23:03:27 -0400

    Add fxml and controller for SettingsScreen

--- client/src/main/java/com/hexanome16/screens/settings/SettingsScreen.java
+++ client/src/main/java/com/hexanome16/screens/settings/SettingsScreen.java
@@ -1,4 +1,31 @@
 package com.hexanome16.screens.settings;
 
-public class SettingsScreen {
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.ui.UI;
+import javafx.scene.paint.Color;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+public class SettingsScreen extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+        gameSettings.setWidth(1280);
+        gameSettings.setHeight(1024);
+        gameSettings.setTitle("Settings screen");
+        gameSettings.setVersion("0.1");
+    }
+
+    @Override
+    protected void initUI() {
+        SettingsScreenUIController uiController = new SettingsScreenUIController();
+        UI ui = getAssetLoader().loadUI("SettingsScreen.fxml", uiController);
+        uiController.doneButton.setOnAction((event) -> getGameController().exit());
+
+        getGameScene().setBackgroundColor(Color.rgb(0,0,5));
+        getGameScene().addUI(ui);
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
