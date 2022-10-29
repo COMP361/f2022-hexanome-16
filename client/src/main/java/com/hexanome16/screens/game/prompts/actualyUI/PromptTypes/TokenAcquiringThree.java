ce2a0a52c36a4394cb7ff0158ce611b9b7fe9623
imad <imad.issafras@mail.mcgill.ca>
2022-10-28 21:13:15 -0400

    Finished acquiring tokens

--- client/src/main/java/com/hexanome16/screens/game/prompts/actualyUI/PromptTypes/TokenAcquiringThree.java
+++ client/src/main/java/com/hexanome16/screens/game/prompts/actualyUI/PromptTypes/TokenAcquiringThree.java
@@ -7,7 +7,6 @@ import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.hexanome16.screens.game.prompts.actualyUI.Components.PromptComponent;
 import java.util.ArrayList;
-import java.util.EnumMap;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
@@ -16,42 +15,19 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 
-public class AssociateBagCard implements PromptTypeInterface {
-  public static enum BonusType{
-    RED,GREEN,BLUE,WHITE,BLACK;
-
-    public static EnumMap<BonusType,Color> m = new EnumMap<BonusType, Color>(BonusType.class);
-
-    static {
-      m.put(RED, Color.RED.darker());
-      m.put(GREEN, Color.GREEN.darker());
-      m.put(BLUE, Color.BLUE.darker());
-      m.put(WHITE, Color.WHITE.darker());
-      m.put(BLACK, Color.BLACK);
-    }
-    public Paint getColor(){
-      return m.get(this);
-    }
-    public Paint getStrokeColor(){
-      if (this == BLACK) return Color.GREY;
-      return Color.BLACK;
-    }
-  }
-
+public class TokenAcquiringThree implements PromptTypeInterface {
   double aWidth = getAppWidth()/2;
   double aHeight = getAppHeight()/2;
-  double aBonusWidth = aWidth/8;
-  double aBonusHeight = aBonusWidth*1.39;
+  double aTokenRadius = aWidth/20;
   double topleftX = (getAppWidth()/2)-(aWidth/2);
   double topleftY = (getAppHeight()/2) - (aHeight/2);
   ArrayList<Node> myNodes = new ArrayList<>();
+  double SelectedTokenTypes = 0;
 
 
   @Override
@@ -82,7 +58,7 @@ public class AssociateBagCard implements PromptTypeInterface {
 
     // prompt message//
     HBox myhBox = new HBox();
-    promptMessage.setText("Choose A Bonus Type");
+    promptMessage.setText("Choose A Token Type");
     promptMessage.setFont(RobotoBoldPrompt);
     promptMessage.setTextAlignment(TextAlignment.CENTER);
     promptMessage.setWrappingWidth(aWidth);
@@ -102,6 +78,7 @@ public class AssociateBagCard implements PromptTypeInterface {
     StackPane button = new StackPane(confirmationButton,confirm);
     button.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
       if(confirmationButton.getOpacity() == 1) {
+        SelectedTokenTypes =0;
         myNodes = new ArrayList<>();
         PromptComponent.closePrompts();
         ////////Handle new Noble AGAIN///////////
@@ -121,11 +98,15 @@ public class AssociateBagCard implements PromptTypeInterface {
 
     // Bonuses /////////////////////////////////////////////////////////////////////////////////////
     bonuses.setAlignment(Pos.CENTER);
-    bonuses.setSpacing(aBonusWidth/4);
-    addBonusType(bonuses,confirmationButton,BonusType.RED);
-    addBonusType(bonuses,confirmationButton,BonusType.GREEN);
-    addBonusType(bonuses,confirmationButton,BonusType.WHITE);
-    addBonusType(bonuses,confirmationButton,BonusType.BLACK);
+    bonuses.setSpacing(aTokenRadius /4);
+
+    addBonusType(bonuses,confirmationButton, AssociateBagCard.BonusType.RED);
+    addBonusType(bonuses,confirmationButton, AssociateBagCard.BonusType.GREEN);
+    addBonusType(bonuses,confirmationButton, AssociateBagCard.BonusType.WHITE);
+    addBonusType(bonuses,confirmationButton, AssociateBagCard.BonusType.BLACK);
+    addBonusType(bonuses,confirmationButton, AssociateBagCard.BonusType.BLUE);
+
+
 
 
     myPrompt.setCenter(bonuses);
@@ -134,12 +115,12 @@ public class AssociateBagCard implements PromptTypeInterface {
     entity.getViewComponent().addChild(myPrompt);
   }
 
-  private void addBonusType(HBox pBonuses, Circle confirmationButton, BonusType pBonusType) {
+  private void addBonusType(HBox pBonuses, Circle confirmationButton, AssociateBagCard.BonusType pBonusType) {
     StackPane myBonus = new StackPane();
-    Rectangle Bonus = new Rectangle(aBonusWidth,aBonusHeight,pBonusType.getColor());
+    Circle Bonus = new Circle(aTokenRadius,pBonusType.getColor());
     Bonus.setStrokeWidth(aHeight/100);
     Bonus.setStroke(pBonusType.getStrokeColor());
-    Rectangle SelectionRectangle = new Rectangle(aBonusWidth*1.1,aBonusHeight*1.1,Color.WHITE);
+    Circle SelectionRectangle = new Circle(aTokenRadius *1.4, Color.WHITE);
     SelectionRectangle.setOpacity(0.5);
     myNodes.add(SelectionRectangle);
 
@@ -159,11 +140,16 @@ public class AssociateBagCard implements PromptTypeInterface {
     });
 
     myBonus.setOnMouseClicked(e -> {
-      for (Node n : myNodes){
-        n.setOpacity(0.5);
+      if (SelectionRectangle.getOpacity() == 1){
+        SelectionRectangle.setOpacity(0.5);
+        SelectedTokenTypes--;
       }
+      else if (SelectedTokenTypes < 3 && (SelectionRectangle.getOpacity()==0.7 || SelectionRectangle.getOpacity()==0.5)) {
       SelectionRectangle.setOpacity(1);
-      confirmationButton.setOpacity(1);
+      SelectedTokenTypes++;
+      }
+      if (SelectedTokenTypes == 3) confirmationButton.setOpacity(1);
+      if (SelectedTokenTypes <3 ) confirmationButton.setOpacity(0.5);
     });
 
     pBonuses.getChildren().add(myBonus);
