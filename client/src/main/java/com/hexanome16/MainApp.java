060cd7c4bb703367c36ea4e1fb4e2fca87488241
Kimi Zhao <94080541+kimikimizz@users.noreply.github.com>
2022-10-30 19:27:51 -0400

    Update MainApp.java

--- client/src/main/java/com/hexanome16/MainApp.java
+++ client/src/main/java/com/hexanome16/MainApp.java
@@ -2,34 +2,43 @@ package com.hexanome16;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import com.almasb.fxgl.dsl.FXGL;
-import com.hexanome16.screens.game.prompts.actualyUI.Components.PromptTypeInterface;
-import com.hexanome16.screens.game.prompts.actualyUI.Components.PromptTypes.BuyCard;
-import com.hexanome16.screens.game.prompts.actualyUI.OpenPromt;
-import com.hexanome16.screens.game.prompts.actualyUI.PromptPartFactory;
+
 import java.util.Map;
 
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+
 public class MainApp extends GameApplication {
-  @Override
-  protected void initSettings(GameSettings gameSettings) {
-      gameSettings.setWidth(1920);
-      gameSettings.setHeight(1080);
-  }
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+        gameSettings.setWidth(1920);
+        gameSettings.setHeight(1080);
+        gameSettings.setTitle("Splendor");
+    }
+    // To Spawn Prompt, OpenPromt.openPrompt(PromptTypeInterface.PromptType.<AN_ENUM_ELEMENT_FROM_PROMPT_TYPE_INTERFACE>);
+    // see all possible prompt types in the PromptTypeInterface, inside the inner enum in src/main/java/com/hexanome16/screens/game/prompts/actualyUI/Components/PromptTypes
+    // Or look for PromptTypeInterface
+    @Override
+    protected void initGame() {
+        backToMainScreen();
+    }
 
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        super.initGameVars(vars);
+    }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-  // To Spawn Prompt, OpenPromt.openPrompt(PromptTypeInterface.PromptType.<AN_ENUM_ELEMENT_FROM_PROMPT_TYPE_INTERFACE>);
-  // see all possible prompt types in the PromptTypeInterface, inside the inner enum in src/main/java/com/hexanome16/screens/game/prompts/actualyUI/Components/PromptTypes
-  // Or look for PromptTypeInterface
-  @Override
-  protected void initGame() {
-    FXGL.getGameWorld().addEntityFactory(new PromptPartFactory());
-  }
+    private void backToMainScreen() {
+        getGameWorld().addEntityFactory(new MainScreen());
+        getGameWorld().addEntityFactory(new LoginScreen());
+        spawn("mainscreen", 0, 0);
+        spawn("diamond", 730, 360);
+        spawn("message", 370, 985);
 
+    }
 
-
-
-  public static void main(String[] args) {
-    launch(args);
-  }
 }
