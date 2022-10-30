efac352bd5dcef2d8f2cd34d92c984dfe11d9466
PeiniCheng <90059889+PeiniCheng@users.noreply.github.com>
2022-10-30 18:35:48 -0400

    save changes

--- client/src/main/java/com/hexanome16/screens/game/prompts/actualyUI/Components/PromptTypes/CustomEvent.java
+++ client/src/main/java/com/hexanome16/screens/game/prompts/actualyUI/Components/PromptTypes/CustomEvent.java
@@ -6,6 +6,7 @@ import javafx.event.EventType;
 public class CustomEvent extends Event {
   public static final EventType<CustomEvent> CLOSING = new EventType<>(Event.ANY,"CLOSING");
 
+  public static final EventType<CustomEvent> BOUGHT = new EventType<>(Event.ANY,"BOUGHT");
   public CustomEvent(EventType<? extends Event> eventType) {
     super(eventType);
   }
