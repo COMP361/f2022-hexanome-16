981c0701bf958af67add41cd1542d63d42d25745
Kimi Zhao <94080541+kimikimizz@users.noreply.github.com>
2022-10-30 20:23:39 -0400

    Add files via upload

--- client/src/main/java/com/hexanome16/screens/startup/StartupScreen.java
+++ client/src/main/java/com/hexanome16/screens/startup/StartupScreen.java
@@ -1,4 +1,70 @@
 package com.hexanome16.screens.startup;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+import com.almasb.fxgl.ui.FXGLButton;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
-public class StartupScreen {
+import static com.almasb.fxgl.dsl.FXGL.spawn;
+
+
+public class StartupScreen implements EntityFactory {
+    @Spawns("mainscreen")
+    public Entity mainScreen(SpawnData data) {
+        var main_screen = FXGL.texture("main_screen.jpg");
+        return FXGL.entityBuilder(data)
+                .view(main_screen)
+                .build();
+    }
+
+    @Spawns("diamond")
+    public Entity diamond(SpawnData data) {
+        FXGLButton button = createButton();
+        return FXGL.entityBuilder(data)
+                .view(button)
+                .build();
+    }
+
+    @Spawns("message")
+    public Entity text(SpawnData data) {
+
+        Text message = createMessage("Click diamond to enter the game!");
+        return FXGL.entityBuilder(data)
+                .view(message)
+                .build();
+    }
+
+    private FXGLButton createButton() {
+        FXGLButton button = new FXGLButton();
+        button.setOnMouseClicked(e -> {
+            spawn("loginscreen", 630, 320);
+            spawn("login", 880, 420);
+
+            spawn("usertext", 830, 525);
+            spawn("username", 1000, 505);
+            spawn("passtext", 830, 575);
+            spawn("password", 1000, 550);
+
+            spawn("button", 920, 635);
+        });
+        button.setPrefSize(500, 260);
+        button.setStyle("-fx-background-color: ffffff00;" );
+        return button;
+    }
+
+    private Text createMessage(String text) {
+        Text message = new Text(text);
+        message.setFont(Font.font("Brush Script MT", FontWeight.BOLD, 115));
+        message.setFill(Paint.valueOf("#FCD828"));
+        message.setStrokeWidth(2.);
+        message.setStroke(Paint.valueOf("#936D35"));
+        message.setStyle("-fx-background-color: ffffff00; ");
+        return message;
+    }
+
 }
