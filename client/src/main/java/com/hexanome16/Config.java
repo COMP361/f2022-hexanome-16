2f98edfd617e281e2ae96589c703fb4d3f50c8e2
Constantin Buruiana <constbur@gmail.com>
2022-10-31 17:42:23 -0400

    removed useless config stuff

--- client/src/main/java/com/hexanome16/Config.java
+++ client/src/main/java/com/hexanome16/Config.java
@@ -6,13 +6,4 @@ public class Config {
 
     public static final String APP_VERSION = "0.0.1";
     public static final String APP_TITLE = "Splendor";
-
-    public static final int CARD_WIDTH = 240;
-    public static final int CARD_HEIGHT = 320;
-
-    public static final int CARD_IMAGE_WIDTH = 200;
-    public static final int CARD_IMAGE_HEIGHT = 200;
-
-    public static final int COST_IMAGE_WIDTH = 64;
-    public static final int COST_IMAGE_HEIGHT = 64;
 }
