c761dabe9be88be9e96c2dd885a79946167d3f08
Imad Issafras <101837444+UnHappySquid@users.noreply.github.com>
2022-11-20 16:30:30 -0500

    Started Checkstyle + Code fix part 5 (Done with View Prompts)

--- client/src/main/java/com/hexanome16/client/screens/game/prompts/components/prompttypes/viewprompts/SeeOtherReserved.java
+++ client/src/main/java/com/hexanome16/client/screens/game/prompts/components/prompttypes/viewprompts/SeeOtherReserved.java
@@ -1,76 +1,27 @@
 package com.hexanome16.client.screens.game.prompts.components.prompttypes.viewprompts;
 
-import static com.almasb.fxgl.dsl.FXGL.getAppHeight;
-import static com.almasb.fxgl.dsl.FXGL.getAppWidth;
-
 import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.texture.Texture;
-import com.hexanome16.client.screens.game.prompts.components.PromptTypeInterface;
-import javafx.geometry.Pos;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SeeOtherReserved implements PromptTypeInterface {
-
-  double aWidth = getAppWidth() / 2;
-  double aHeight = getAppHeight() / 2;
-  double aCardWidth = aWidth / 4;
-  double aCardHeight = aCardWidth * 1.39;
-  double topleftX = (getAppWidth() / 2) - (aWidth / 2);
-  double topleftY = (getAppHeight() / 2) - (aHeight / 2);
+/**
+ * A class responsible for populating See Other reserved Cards prompt.
+ */
+public class SeeOtherReserved extends SeeReservedAbstract {
 
   @Override
-  public double width() {
-    return aWidth;
+  protected void appendBehaviour(Texture t) {
+    // we don't want to add behaviour for now
+    return;
   }
 
   @Override
-  public double height() {
-    return aHeight;
+  protected void promptOpened() {
+    // hardCoded for now
+    assert (hiddenCards + viewAbleCards.size()) < 3;
+    hiddenCards = 2;
+    viewAbleCards = new ArrayList<>(List.of(FXGL.texture("card1.png")));
   }
 
-  @Override
-  public void populatePrompt(Entity entity) {
-    BorderPane myBorderPane = new BorderPane();
-    myBorderPane.setTranslateX(topleftX);
-    myBorderPane.setTranslateY(topleftY);
-    HBox ReservedCards = new HBox();
-    ReservedCards.setAlignment(Pos.CENTER);
-    ReservedCards.setSpacing((aWidth - 3 * aCardWidth) / 4);
-    Text myPromptMessage = new Text("Opponent Reserved Cards");
-    myPromptMessage.setFont(Font.font(aHeight / 20));
-    myPromptMessage.setTextAlignment(TextAlignment.CENTER);
-    myPromptMessage.setWrappingWidth(aWidth);
-    myBorderPane.setTop(myPromptMessage);
-    myBorderPane.setCenter(ReservedCards);
-    //    myScrollPane.setTranslateX(topleftX);
-//    myScrollPane.setTranslateY(topleftY);
-
-
-    Texture myCard = FXGL.texture("card1.png");
-    myCard.setFitWidth(aCardWidth);
-    myCard.setFitHeight(aCardHeight);
-    ReservedCards.getChildren().add(myCard);
-
-    StackPane myanonymousCard = new StackPane();
-    Rectangle myOtherCard = new Rectangle(aCardWidth, aCardHeight, Color.BLACK);
-    Text myintoregation = new Text("?");
-    myintoregation.setFont(Font.font(aCardHeight * 0.9));
-    myintoregation.setFill(Color.WHITE);
-    myintoregation.setTextAlignment(TextAlignment.CENTER);
-    myanonymousCard.getChildren().addAll(myOtherCard, myintoregation);
-    ReservedCards.getChildren().add(myanonymousCard);
-
-    ReservedCards.setPrefSize(aWidth, aHeight * 0.8);
-
-
-    entity.getViewComponent().addChild(myBorderPane);
-  }
 }
