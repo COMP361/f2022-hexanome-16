0d1c1766ea323b34c0d1d2492a64efb3a0396f79
Constantin Buruiana <constbur@gmail.com>
2022-11-22 21:27:26 -0500

    character escaping please die a slow and painful death by torture

--- client/src/main/java/com/hexanome16/client/utils/UrlUtils.java
+++ client/src/main/java/com/hexanome16/client/utils/UrlUtils.java
@@ -3,8 +3,10 @@ package com.hexanome16.client.utils;
 import com.almasb.fxgl.core.asset.AssetType;
 import com.almasb.fxgl.core.collection.PropertyMap;
 import com.almasb.fxgl.dsl.FXGL;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -36,10 +38,7 @@ public class UrlUtils {
    * @return the decoded String
    */
   public static String decodeUriComponent(String s) {
-    return s == null ? null : s.isBlank() ? "" : URLDecoder.decode(
-        s.replaceAll("%", "%25"),
-        StandardCharsets.UTF_8
-    );
+    return s == null ? null : s.isBlank() ? "" : URLDecoder.decode(s, StandardCharsets.UTF_8);
   }
 
   /**
@@ -51,10 +50,7 @@ public class UrlUtils {
    * @return the encoded String
    */
   public static String encodeUriComponent(String s) {
-    return s == null ? null : s.isBlank() ? "" : URLEncoder.encode(s, StandardCharsets.UTF_8)
-        .replaceAll(" ", "%20")
-        .replaceAll("%25", "%")
-        .replaceAll("%26", "&");
+    return s == null ? null : s.isBlank() ? "" : URLEncoder.encode(s, StandardCharsets.UTF_8);
   }
 
   //TODO: Add ability to set custom URLs for LS/Game Server.
@@ -68,16 +64,20 @@ public class UrlUtils {
    */
   public static URI createLobbyServiceUri(String path, String query) {
     try {
+      String urlString = LS_PROPERTIES.getString("server.protocol") + "://"
+          + LS_PROPERTIES.getString("server.host") + ":"
+          + LS_PROPERTIES.getInt("server.port") + path + "?" + query;
+      URL url = new URL(decodeUriComponent(urlString));
       return new URI(
-          LS_PROPERTIES.getString("server.protocol"),
-          null,
-          LS_PROPERTIES.getString("server.host"),
-          LS_PROPERTIES.getInt("server.port"),
-          path == null || path.isBlank() ? null : path,
-          query == null || query.isBlank() ? null : query,
-          null
+          url.getProtocol(),
+          url.getUserInfo(),
+          url.getHost(),
+          url.getPort(),
+          url.getPath(),
+          url.getQuery(),
+          url.getRef()
       );
-    } catch (URISyntaxException e) {
+    } catch (URISyntaxException | MalformedURLException e) {
       e.printStackTrace();
       return null;
     }
@@ -92,16 +92,20 @@ public class UrlUtils {
    */
   public static URI createGameServerUri(String path, String query) {
     try {
+      String urlString = SERVER_PROPERTIES.getString("server.protocol") + "://"
+          + SERVER_PROPERTIES.getString("server.host") + ":"
+          + SERVER_PROPERTIES.getInt("server.port") + path + "?" + query;
+      URL url = new URL(decodeUriComponent(urlString));
       return new URI(
-          SERVER_PROPERTIES.getString("server.protocol"),
-          null,
-          SERVER_PROPERTIES.getString("server.host"),
-          SERVER_PROPERTIES.getInt("server.port"),
-          path == null || path.isBlank() ? null : path,
-          query == null || query.isBlank() ? null : query,
-          null
+          url.getProtocol(),
+          url.getUserInfo(),
+          url.getHost(),
+          url.getPort(),
+          url.getPath(),
+          url.getQuery(),
+          url.getRef()
       );
-    } catch (URISyntaxException e) {
+    } catch (URISyntaxException | MalformedURLException e) {
       e.printStackTrace();
       return null;
     }
