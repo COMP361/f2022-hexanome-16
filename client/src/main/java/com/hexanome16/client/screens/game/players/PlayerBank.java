cf6f8c2fc0bb35c9f118f90654ad5cd5b6f4d06c
Kimi Zhao <94080541+kimikimizz@users.noreply.github.com>
2022-11-26 18:24:51 -0500

    Save

---
+++ client/src/main/java/com/hexanome16/client/screens/game/players/PlayerBank.java
@@ -0,0 +1,33 @@
+package com.hexanome16.client.screens.game.players;
+
+public class PlayerBank {
+    public int numOfDiamond;
+    public int numOfRuby ;
+    public int numOfSapphire;
+    public int numOfEmerald ;
+    public int numOfOnyx;
+    public int numOfGold;
+
+    public PlayerBank() {
+        numOfDiamond = 2;
+        numOfRuby = 2;
+        numOfSapphire = 2;
+        numOfEmerald = 2;
+        numOfOnyx = 2;
+        numOfGold = 2;
+    }
+
+    public void acquireTokenSameColor(){
+
+    }
+
+    public void acquireTokenDiffColor(){
+
+    }
+
+    public void purchaseCard() {
+
+    }
+
+
+}
