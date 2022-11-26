3bd775084771f12e4fbf89ebca98c1f8dcc3f7d5
eleadufresne <elea.dufresne@mail.mcgill.ca>
2022-11-26 18:13:28 -0500

    started on inventory backend

--- server/src/main/java/com/hexanome16/server/models/Player.java
+++ server/src/main/java/com/hexanome16/server/models/Player.java
@@ -6,16 +6,14 @@ package com.hexanome16.server.models;
 public class Player {
   private final String username;
   private PlayerBank bank;
+  public PlayerBank getBank() { return bank; }
+  public void setBank(PlayerBank bank) { this.bank = bank; }
 
   public Player(String username) {
     this.username = username;
   }
-
   public String getUsername() {
     return username;
   }
 
-  public PlayerBank getBank() {
-    return bank;
-  }
 }
