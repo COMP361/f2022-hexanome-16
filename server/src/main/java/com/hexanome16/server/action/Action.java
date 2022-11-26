89f0946691c1aaa2fb2ba44a5416c4a9dd079899
PeiniCheng <peini.cheng@mail.mcgill.ca>
2022-11-26 16:46:42 -0500

    delete black board

--- server/src/main/java/com/hexanome16/server/action/Action.java
+++
@@ -1,7 +0,0 @@
-package com.hexanome16.server.action;
-
-/**
- * Action interface.
- */
-public interface Action {
-}
--- server/src/main/java/com/hexanome16/server/action/generator/ActionGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.hexanome16.server.action.generator;
-
-/**
- * Action generator class.
- */
-public interface ActionGenerator {
-
-}
