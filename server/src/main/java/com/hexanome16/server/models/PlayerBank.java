1c47ab3a558c98e8980eec42ce0409816e5eaca6
Kimi Zhao <94080541+kimikimizz@users.noreply.github.com>
2022-11-26 22:50:41 -0500

    Refactored some methods

--- server/src/main/java/com/hexanome16/server/models/PlayerBank.java
+++ server/src/main/java/com/hexanome16/server/models/PlayerBank.java
@@ -7,21 +7,13 @@ import java.util.Hashtable;
  * Player Bank for each player.
  */
 public class PlayerBank extends Bank {
-  public Hashtable<Gem, ArrayList<Token>> playerBank;
+  private final Hashtable<Gem, ArrayList<Token>> playerBank;
 
   /**
    * The constructor gives each player two tokens to start with.
    */
   public PlayerBank() {
-    playerBank = new Hashtable<Gem, ArrayList<Token>>();
-
-    playerBank.put(Gem.RUBY, addTokenToList(null, Gem.RUBY, 3));
-    playerBank.put(Gem.EMERALD, addTokenToList(null, Gem.EMERALD, 3));
-    playerBank.put(Gem.SAPPHIRE, addTokenToList(null, Gem.SAPPHIRE, 3));
-    playerBank.put(Gem.DIAMOND, addTokenToList(null, Gem.DIAMOND, 3));
-    playerBank.put(Gem.ONYX, addTokenToList(null, Gem.ONYX, 3));
-    playerBank.put(Gem.GOLD, addTokenToList(null, Gem.GOLD, 3));
-
+    playerBank = initBank(3);
   }
 
   /**
@@ -30,9 +22,9 @@ public class PlayerBank extends Bank {
    * @param gem = Gem type (RUBY, DIAMOND, etc) associated with the token.
    */
   public void acquireTokenSameColor(Gem gem) {
-    ArrayList<Token> currentList = playerBank.get(gem);
-    currentList = addTokenToList(currentList, gem, 2);
-    playerBank.replace(gem, currentList);
+//    ArrayList<Token> currentList = playerBank.get(gem);
+//    addTokensToList(currentList, Game.getGameMap().get(sessionId).
+//    playerBank.replace(gem, currentList);
   }
 
   /**
@@ -43,32 +35,22 @@ public class PlayerBank extends Bank {
    * @param gem3 = Gem type (RUBY, DIAMOND, etc) associated with the token.
    */
   public void acquireTokenDiffColor(Gem gem1, Gem gem2, Gem gem3) {
-    ArrayList<Token> currentList1 = playerBank.get(gem1);
-    currentList1 = addTokenToList(currentList1, gem1, 1);
-    playerBank.replace(gem1, currentList1);
-
-    ArrayList<Token> currentList2 = playerBank.get(gem2);
-    currentList2 = addTokenToList(currentList2, gem2, 1);
-    playerBank.replace(gem2, currentList2);
-
-    ArrayList<Token> currentList3 = playerBank.get(gem3);
-    currentList3 = addTokenToList(currentList3, gem3, 1);
-    playerBank.replace(gem3, currentList3);
+//    ArrayList<Token> currentList1 = playerBank.get(gem1);
+//    currentList1 = initBank(currentList1, gem1, 1);
+//    playerBank.replace(gem1, currentList1);
+//
+//    ArrayList<Token> currentList2 = playerBank.get(gem2);
+//    currentList2 = initBank(currentList2, gem2, 1);
+//    playerBank.replace(gem2, currentList2);
+//
+//    ArrayList<Token> currentList3 = playerBank.get(gem3);
+//    currentList3 = initBank(currentList3, gem3, 1);
+//    playerBank.replace(gem3, currentList3);
   }
 
   public void purchaseCard() {
 
   }
 
-  private ArrayList<Token> addTokenToList(ArrayList<Token> list, Gem gem, int amount) {
-    // Initialize an ArrayList if no list was provided
-    if (list == null) {
-      list = new ArrayList<>();
-    }
-    for (int i = 0; i < amount; i++) {
-      list.add(new Token(gem));
-    }
-    return list;
-  }
 
 }
