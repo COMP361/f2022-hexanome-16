5eb3465ad8ac1a4f400aacc5e2cd01f2b88dbd3e
Imad Issafras <101837444+UnHappySquid@users.noreply.github.com>
2022-11-28 16:58:29 -0500

    added elea's part

--- server/src/main/java/com/hexanome16/server/controllers/InventoryController.java
+++ server/src/main/java/com/hexanome16/server/controllers/InventoryController.java
@@ -5,28 +5,35 @@ import com.hexanome16.server.models.Inventory;
 import com.hexanome16.server.models.LevelCard;
 import com.hexanome16.server.models.Noble;
 import com.hexanome16.server.models.PlayerBank;
+import java.util.List;
+import javax.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.List;
-
+/**
+ * Rest Controller for inventory stuff.
+ */
 @RestController
 public class InventoryController {
+  PlayerBank playerBank;
+  List<Noble> ownedNobles;
+  List<Noble> reservedNobles;
+  List<LevelCard> ownedCards;
+  LevelCard[] reservedCards = new LevelCard[3];
 
-    PlayerBank playerBank;
-    List<Noble> ownedNobles;
-    List<Noble> reservedNobles;
-    List<LevelCard> ownedCards;
-    LevelCard[] reservedCards = new LevelCard[3];
 
-
-    @PostMapping(value = { "/games/{sessionId}/{playerId}/inventory/create"})
-    public InventoryDto createInventory(@Valid @RequestBody InventoryDto request){
-        // new Inventory
-        Inventory inventory = new Inventory();
-        return null;
-    }
+  /**
+   * Request to create inventory.
+   *
+   * @param request requested inventory
+   * @return null
+   */
+  @PostMapping(value = {"/games/{sessionId}/{playerId}/inventory/create"})
+  public InventoryDto createInventory(@Valid @RequestBody InventoryDto request) {
+    // new Inventory
+    Inventory inventory = new Inventory();
+    return null;
+  }
 
 }
