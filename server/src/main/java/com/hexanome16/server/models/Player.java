3bfd13961b40be67bb92e2a8ae833750a889184b
eleadufresne <elea.dufresne@mail.mcgill.ca>
2022-11-28 16:32:14 -0500

    checkstyle bs

--- server/src/main/java/com/hexanome16/server/models/Player.java
+++ server/src/main/java/com/hexanome16/server/models/Player.java
@@ -9,8 +9,14 @@ public class Player {
   private final String name;
   private final String preferredColour;
   private Inventory inventory; // the player has an inventory, not a bank
-  public Inventory getInventory() { return this.inventory; }
-  public void setInventory(Inventory inventory) { this.inventory = inventory; }
+
+  public Inventory getInventory() {
+    return this.inventory;
+  }
+
+  public void setInventory(Inventory inventory) {
+    this.inventory = inventory;
+  }
 
   @JsonCreator
   public Player(String name, String preferredColour) {
@@ -26,6 +32,18 @@ public class Player {
     return preferredColour;
   }
 
-  
+  /** Add this card to the player's inventory.
+   *
+   * @return true on success */
+  public boolean addCardToInventory(DevelopmentCard developmentCard) {
+    return developmentCard.addToInventory(this.inventory);
+  }
+
+  /** Reserve this card.
+   *
+   * @return true on success */
+  public boolean reserveCard(DevelopmentCard developmentCard) {
+    return developmentCard.reserveCard(this.inventory);
+  }
 
 }
