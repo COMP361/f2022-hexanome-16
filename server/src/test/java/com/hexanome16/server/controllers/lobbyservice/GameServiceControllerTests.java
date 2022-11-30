7e085b78964ca00742cc5749dcca190dad78f90d
Constantin Buruiana <constbur@gmail.com>
2022-11-29 23:00:46 -0500

    added game service tests

--- server/src/test/java/com/hexanome16/server/controllers/lobbyservice/GameServiceControllerTests.java
+++ server/src/test/java/com/hexanome16/server/controllers/lobbyservice/GameServiceControllerTests.java
@@ -1,5 +1,7 @@
 package com.hexanome16.server.controllers.lobbyservice;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.hexanome16.server.controllers.lobbyservice.auth.AuthController;
 import com.hexanome16.server.controllers.lobbyservice.gameservice.GameServiceController;
 import com.hexanome16.server.models.sessions.GameParams;
@@ -9,9 +11,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.MockRestServiceServerAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+/**
+ * This class tests the GameServiceController.
+ * These aren't exactly unit tests since they request the external Lobby Service
+ * but hey code coverage is code coverage :)
+ */
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {UrlUtils.class, GameParams.class, AuthController.class,
     GameServiceController.class})
@@ -22,6 +30,13 @@ public class GameServiceControllerTests {
 
   @Test
   public void createGameService() {
+    ResponseEntity<Void> response = gameServiceController.createGameService();
+    assertTrue(response.getStatusCode().is2xxSuccessful());
+  }
 
+  @Test
+  public void deleteGameService() {
+    ResponseEntity<Void> response = gameServiceController.deleteGameService();
+    assertTrue(response.getStatusCode().is2xxSuccessful());
   }
 }
