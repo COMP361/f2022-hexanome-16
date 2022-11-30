0672254ccde1c3093bd98abe9612d4198a232e2b
PeiniCheng <peini.cheng@mail.mcgill.ca>
2022-11-29 22:44:03 -0500

    delete unused comment block

--- server/src/main/java/com/hexanome16/server/models/user/User.java
+++ server/src/main/java/com/hexanome16/server/models/user/User.java
@@ -1,11 +1,8 @@
 package com.hexanome16.server.models.user;
 
-import java.util.Objects;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -22,20 +19,4 @@ public final class User {
   private final String password;
   private final String role;
   private String preferredColour;
-
-//  /**
-//   * Constructor.
-//   *
-//   * @param name            The name of the user.
-//   * @param password        The password of the user.
-//   * @param preferredColour The preferred colour of the user.
-//   * @param role            The role of the user.
-//   */
-//  public User(String name, String password, String role, String preferredColour) {
-//    this.name = name;
-//    this.password = password;
-//    this.role = role;
-//    this.preferredColour = preferredColour;
-//  }
-
 }
