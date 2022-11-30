6dadcbd26b3151e6b2d567022d665a151cbd6d6e
PeiniCheng <peini.cheng@mail.mcgill.ca>
2022-11-29 23:06:18 -0500

    lombok for dto and a controller test

--- server/src/main/java/com/hexanome16/server/dto/CardJson.java
+++ server/src/main/java/com/hexanome16/server/dto/CardJson.java
@@ -1,88 +1,30 @@
 package com.hexanome16.server.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * For converting json file to level card objects.
  * Only used for creating cards at the start of the game.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CardJson {
-  String level;
-  String bonus;
+  private String level;
+  private String bonus;
 
-  int id;
-  int prestigePoint;
+  private int id;
+  private int prestigePoint;
 
-  int onyxAmount;
+  private int onyxAmount;
 
-  int sapphireAmount;
+  private int sapphireAmount;
 
-  int emeraldAmount;
+  private int emeraldAmount;
 
-  int rubyAmount;
+  private int rubyAmount;
 
-  int diamondAmount;
-
-  public CardJson() {
-  }
-
-  /**
-   * Json to java object.
-   *
-   * @param level          card level
-   * @param bonus          card bonus
-   * @param prestigePoint  card prestige point
-   * @param onyxAmount     onyx amount
-   * @param sapphireAmount sapphire amount
-   * @param emeraldAmount  emerald amount
-   * @param rubyAmount     ruby amount
-   * @param diamondAmount  diamond amount
-   */
-  public CardJson(int id, String level, String bonus, int prestigePoint, int onyxAmount,
-                  int sapphireAmount,
-                  int emeraldAmount, int rubyAmount, int diamondAmount) {
-    this.id = id;
-    this.level = level;
-    this.bonus = bonus;
-    this.prestigePoint = prestigePoint;
-    this.onyxAmount = onyxAmount;
-    this.sapphireAmount = sapphireAmount;
-    this.emeraldAmount = emeraldAmount;
-    this.rubyAmount = rubyAmount;
-    this.diamondAmount = diamondAmount;
-  }
-
-  public String getLevel() {
-    return level;
-  }
-
-  public String getBonus() {
-    return bonus;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public int getPrestigePoint() {
-    return prestigePoint;
-  }
-
-  public int getOnyxAmount() {
-    return onyxAmount;
-  }
-
-  public int getSapphireAmount() {
-    return sapphireAmount;
-  }
-
-  public int getEmeraldAmount() {
-    return emeraldAmount;
-  }
-
-  public int getRubyAmount() {
-    return rubyAmount;
-  }
-
-  public int getDiamondAmount() {
-    return diamondAmount;
-  }
+  private int diamondAmount;
 }
