3c22c58c7ffbeb71b5532744ef3d608cd2b2ee3f
Kimi Zhao <94080541+kimikimizz@users.noreply.github.com>
2022-11-30 10:42:09 -0500

    Save

--- server/src/test/java/com/hexanome16/server/ServerApplicationTests.java
+++ server/src/test/java/com/hexanome16/server/ServerApplicationTests.java
@@ -4,6 +4,8 @@ import com.hexanome16.server.models.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.junit.Assert.*;
 
 
@@ -64,17 +66,36 @@ class ServerApplicationTests {
   public void testInitBagBonusGem() {
     BagBonus bagBonusTest = new BagBonus(BonusType.CARD_BONUS);
     bagBonusTest.setChosenGem(Gem.RUBY);
-    assertEquals(Gem.RUBY, bagBonusTest.getChosenGem());
+    assertEquals(Optional.of(Gem.RUBY), bagBonusTest.getChosenGem());
+  }
+
+  @Test
+  public void testInitDeck() {
+    Deck deckTest = new Deck();
+    assertEquals(true, deckTest.isEmpty());
   }
 
   @Test
   public void testDeckShuffle() {
     Deck deckTest1 = new Deck();
-    deckTest1.shuffle();
+    addCardsToDeckAndShuffle(deckTest1, 10);
     Deck deckTest2 = new Deck();
-    deckTest2.shuffle();
-    System.out.println(deckTest1.isSameDeck(deckTest2));
-    assertFalse(deckTest1.isSameDeck(deckTest2));
+    addCardsToDeckAndShuffle(deckTest2, 10);
+    assertEquals(false, deckTest1.isSameDeck(deckTest2));
   }
 
+  private void addCardsToDeckAndShuffle(Deck deck, int amount) {
+    for (int i = 0; i < amount; i++) {
+      deck.addCard(new LevelCard(i, 0, "", new CardPrice(), Level.ONE ));
+    }
+    deck.shuffle();
+  }
+  @Test
+  public void testDeckRemainingCards() {
+    Deck deckTest = new Deck();
+    addCardsToDeckAndShuffle(deckTest, 10);
+    deckTest.nextCard();
+    deckTest.nextCard();
+    assertEquals(8, deckTest.remainingAmount());
+  }
 }
