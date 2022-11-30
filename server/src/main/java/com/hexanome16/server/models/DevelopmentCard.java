eb7f5c4db6a457d643ed3913ab7336e7284cbf17
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2022-11-29 21:51:49 -0500

    Add lombok to models

--- server/src/main/java/com/hexanome16/server/models/DevelopmentCard.java
+++ server/src/main/java/com/hexanome16/server/models/DevelopmentCard.java
@@ -1,46 +1,21 @@
 package com.hexanome16.server.models;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 /**
  * Development card class.
  */
+@Data
 public abstract class DevelopmentCard {
   private final int id;
   private final int prestigePoint;
 
   private final String texturePath;
   private final Price price;
-
-  /**
-   * Creates a new card object with id, prestigePoint, texturePath and price.
-   *
-   * @param id            card id
-   * @param prestigePoint card prestige point
-   * @param texturePath   card texture
-   * @param price         card price
-   */
-  public DevelopmentCard(int id, int prestigePoint, String texturePath, Price price) {
-    this.id = id;
-    this.prestigePoint = prestigePoint;
-    this.price = price;
-    this.texturePath = texturePath;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public int getPrestigePoint() {
-    return prestigePoint;
-  }
-
-  public Price getPrice() {
-    return price;
-  }
-
-  public String getTexturePath() {
-    return texturePath;
-  }
-
 
   /** Add this card to the player's inventory.
    *
--- server/src/main/java/com/hexanome16/server/models/TokenStack.java
+++ server/src/main/java/com/hexanome16/server/models/TokenStack.java
@@ -2,26 +2,19 @@ package com.hexanome16.server.models;
 
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 /**
  * A stack of tokens of the same color.
  */
+@Getter
+@AllArgsConstructor
 public class TokenStack {
   private final Gem gem;
-  private final List<Token> tokenList;
-
-  public TokenStack(Gem gem) {
-    this.gem = gem;
-    this.tokenList = new ArrayList<Token>();
-  }
-
-  public Gem getGem() {
-    return gem;
-  }
-
-  public List<Token> getTokenList() {
-    return tokenList;
-  }
+  private final List<Token> tokenList = new ArrayList<>();
 
   public void addToken(Token token) {
     tokenList.add(token);
