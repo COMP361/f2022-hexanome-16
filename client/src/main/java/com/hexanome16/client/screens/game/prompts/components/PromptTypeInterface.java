977c89015aea479eab8b4a2cc7028fdde6220b59
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2022-11-30 21:16:48 -0500

    Update enums
    Make enums have attributes instead of using a map.
    Improves readability and extendability

--- client/src/main/java/com/hexanome16/client/screens/game/prompts/components/PromptTypeInterface.java
+++ client/src/main/java/com/hexanome16/client/screens/game/prompts/components/PromptTypeInterface.java
@@ -16,7 +16,6 @@ import com.hexanome16.client.screens.game.prompts.components.prompttypes.otherch
 import com.hexanome16.client.screens.game.prompts.components.prompttypes.viewprompts.SeeCards;
 import com.hexanome16.client.screens.game.prompts.components.prompttypes.viewprompts.SeeOtherReserved;
 import com.hexanome16.client.screens.game.prompts.components.prompttypes.viewprompts.SeeOwnReserved;
-import java.util.EnumMap;
 import java.util.function.Predicate;
 import javafx.scene.Node;
 
@@ -28,15 +27,18 @@ import javafx.scene.Node;
  */
 public interface PromptTypeInterface {
 
+  /**
+   * The constant nullPromptType.
+   */
   PromptTypeInterface nullPromptType = new PromptTypeInterface() {
 
     @Override
-    public double width() {
+    public double getWidth() {
       return 100;
     }
 
     @Override
-    public double height() {
+    public double getHeight() {
       return 100;
     }
 
@@ -46,31 +48,10 @@ public interface PromptTypeInterface {
   };
 
   /**
-   * Getter method to access prompt width.
-   *
-   * @return The prompt's width.
-   */
-  double width();
-
-  /**
-   * Getter method to access prompt height.
-   *
-   * @return The prompt's height.
-   */
-  double height();
-
-  /**
-   * Method which populates the prompt with elements specific to the prompt type.
-   *
-   * @param entity entity associated to the prompt component that called this method.
-   */
-  void populatePrompt(Entity entity);
-
-  /**
    * Changes the opacity of affected Node to upperOpacity when mouse enters hoveredOver,
    * changes the opacity of affected Node to lowerOpacity when mouse leaves hoveredOver.
    *
-   * @param hoveredOver Node that the mouse is hovering Over.
+   * @param hoveredOver  Node that the mouse is hovering Over.
    * @param affectedNode Node affected by the hover.
    * @param lowerOpacity Opacity of affectedNode after mouse stops hovering over hoveredNode.
    * @param upperOpacity Opacity of affectedNode after mouse starts hovering over hoveredNode.
@@ -90,12 +71,12 @@ public interface PromptTypeInterface {
    * Changes the opacity of affected Node to upperOpacity when mouse enters hoveredOver,
    * changes the opacity of affected Node to lowerOpacity when mouse leaves hoveredOver.
    *
-   * @param hoveredOver Node that the mouse is hovering Over.
+   * @param hoveredOver  Node that the mouse is hovering Over.
    * @param affectedNode Node affected by the hover.
    * @param lowerOpacity Opacity of affectedNode after mouse stops hovering over hoveredNode.
    * @param upperOpacity Opacity of affectedNode after mouse starts hovering over hoveredNode.
-   * @param condition a Predicate object which makes the changes happen only if it is true.
-   * @param toTest the Object to test for the condition
+   * @param condition    a Predicate object which makes the changes happen only if it is true.
+   * @param toTest       the Object to test for the condition
    */
   static void setOnHoverEffectOpacity(Node hoveredOver, Node affectedNode,
                                       double lowerOpacity, double upperOpacity,
@@ -113,45 +94,115 @@ public interface PromptTypeInterface {
   }
 
   /**
+   * Getter method to access prompt width.
+   *
+   * @return The prompt's width.
+   */
+  double getWidth();
+
+  /**
+   * Getter method to access prompt height.
+   *
+   * @return The prompt's height.
+   */
+  double getHeight();
+
+  /**
+   * Method which populates the prompt with elements specific to the prompt type.
+   *
+   * @param entity entity associated to the prompt component that called this method.
+   */
+  void populatePrompt(Entity entity);
+
+  /**
    * Enum of all the possible types of prompts.
    */
   enum PromptType {
-    TOKEN_ACQUIRING,          // Main
-    BUY_CARDS,                // Main
-    SEE_OTHER_RESERVED,       // Main
-    SEE_OWN_RESERVED,         // Main
-    SEE_CARDS,                // Main
-    CHOOSE_NOBLES,            // Main
-    BUY_CARDS_BY_CARDS,       // Main
-    BUY_BAG_CARD,             // Main
-    BUY_RESERVE_NOBLE_CARD,   // Main
-    PAUSE,                    // Main
-    BUYING_RESERVED,          // Helper
-    TOKEN_ACQUIRING_TWO,      // Helper
-    TOKEN_ACQUIRING_THREE,    // Helper
-    ASSOCIATE_BAG_CARD,       // Helper
-    CHOOSE_NOBLE_TO_RESERVE,  // Helper
-    NULL;                     // NullObject
+    /**
+     * Token acquiring prompt type.
+     * <p><i>Main</i></p>
+     */
+    TOKEN_ACQUIRING(new TokenAcquiring()),
+    /**
+     * Token acquiring two prompt type.
+     * <p><i>Helper</i></p>
+     */
+    TOKEN_ACQUIRING_TWO(new TokenAcquiringTwo()),
+    /**
+     * Token acquiring three prompt type.
+     * <p><i>Helper</i></p>
+     */
+    TOKEN_ACQUIRING_THREE(new TokenAcquiringThree()),
+    /**
+     * Buy cards prompt type.
+     * <p><i>Main</i></p>
+     */
+    BUY_CARDS(new BuyCardPrompt()),
+    /**
+     * See other reserved prompt type.
+     * <p><i>Main</i></p>
+     */
+    SEE_OTHER_RESERVED(new SeeOtherReserved()),
+    /**
+     * See own reserved prompt type.
+     * <p><i>Main</i></p>
+     */
+    SEE_OWN_RESERVED(new SeeOwnReserved()),
+    /**
+     * See cards prompt type.
+     * <p><i>Main</i></p>
+     */
+    SEE_CARDS(new SeeCards()),
+    /**
+     * Choose nobles prompt type.
+     * <p><i>Main</i></p>
+     */
+    CHOOSE_NOBLES(new ChooseNoble()),
+    /**
+     * Buy cards by cards prompt type.
+     * <p><i>Main</i></p>
+     */
+    BUY_CARDS_BY_CARDS(new BuyCardByCard()),
+    /**
+     * Buy bag card prompt type.
+     * <p><i>Main</i></p>
+     */
+    BUY_BAG_CARD(new BuyingBagCard()),
+    /**
+     * Buy reserve noble card prompt type.
+     * <p><i>Main</i></p>
+     */
+    BUY_RESERVE_NOBLE_CARD(new BuyReserveNobleCard()),
+    /**
+     * Pause prompt type.
+     * <p><i>Main</i></p>
+     */
+    PAUSE(new Pause()),
+    /**
+     * Buying reserved prompt type.
+     * <p><i>Main</i></p>
+     */
+    BUYING_RESERVED(new BuyingReserved()),
+    /**
+     * Associate bag card prompt type.
+     * <p><i>Helper</i></p>
+     */
+    ASSOCIATE_BAG_CARD(new AssociateBagCard()),
+    /**
+     * Choose noble to reserve prompt type.
+     * <p><i>Helper</i></p>
+     */
+    CHOOSE_NOBLE_TO_RESERVE(new ChooseNobleReserve()),
+    /**
+     * Null prompt type.
+     * <p><i>NullObject</i></p>
+     */
+    NULL(nullPromptType);
 
-    static final EnumMap<PromptType, PromptTypeInterface> myMap = new EnumMap<>(PromptType.class);
+    private final PromptTypeInterface promptType;
 
-    static {
-      myMap.put(TOKEN_ACQUIRING, new TokenAcquiring());
-      myMap.put(TOKEN_ACQUIRING_TWO, new TokenAcquiringTwo());
-      myMap.put(TOKEN_ACQUIRING_THREE, new TokenAcquiringThree());
-      myMap.put(BUY_CARDS, new BuyCardPrompt());
-      myMap.put(SEE_OTHER_RESERVED, new SeeOtherReserved());
-      myMap.put(SEE_OWN_RESERVED, new SeeOwnReserved());
-      myMap.put(SEE_CARDS, new SeeCards());
-      myMap.put(CHOOSE_NOBLES, new ChooseNoble());
-      myMap.put(BUYING_RESERVED, new BuyingReserved());
-      myMap.put(BUY_CARDS_BY_CARDS, new BuyCardByCard());
-      myMap.put(ASSOCIATE_BAG_CARD, new AssociateBagCard());
-      myMap.put(BUY_BAG_CARD, new BuyingBagCard());
-      myMap.put(BUY_RESERVE_NOBLE_CARD, new BuyReserveNobleCard());
-      myMap.put(CHOOSE_NOBLE_TO_RESERVE, new ChooseNobleReserve());
-      myMap.put(PAUSE, new Pause());
-      myMap.put(NULL, nullPromptType);
+    PromptType(PromptTypeInterface promptType) {
+      this.promptType = promptType;
     }
 
     /**
@@ -160,7 +211,7 @@ public interface PromptTypeInterface {
      * @return A flyweight-like instance of the class associated to that type.
      */
     public PromptTypeInterface getAssociatedClass() {
-      return myMap.get(this);
+      return this.promptType;
     }
   }
 }
