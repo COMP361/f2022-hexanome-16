3cc12fda4aaffca00a734984cc4ca0f7a467a9fa
Constantin Buruiana <constbur@gmail.com>
2023-01-17 15:01:44 -0500

    added verifyplayer tests

--- server/src/test/java/com/hexanome16/server/services/auth/AuthServiceTest.java
+++ server/src/test/java/com/hexanome16/server/services/auth/AuthServiceTest.java
@@ -1,28 +1,15 @@
 package com.hexanome16.server.services.auth;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertFalse;
 import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 import com.hexanome16.server.controllers.DummyAuthService;
 import com.hexanome16.server.models.auth.TokensInfo;
 import com.hexanome16.server.services.DummyAuths;
-import com.hexanome16.server.util.UrlUtils;
-import java.net.URI;
-import java.util.Collections;
 import java.util.Objects;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Tests for {@link AuthService}.
@@ -41,69 +28,11 @@ public class AuthServiceTest {
       DummyAuths.validTokensInfos.get(0).getAccessToken(),
       DummyAuths.invalidTokensInfos.get(0).getAccessToken()
   };
+  private final Long[] sessionIds = {
+      DummyAuths.validSessionIds.get(0),
+      DummyAuths.invalidSessionIds.get(0)
+  };
   private final DummyAuthService authService = new DummyAuthService();
-
-  /*private URI createMockedUri() {
-    String baseUrl = "http://localhost:4242";
-    return URI.create(
-        "%s%s?%s".formatted(baseUrl, argThat((String s) -> s.contains("oauth")),
-            argThat((String s) -> s.contains("="))));
-  }
-
-  @BeforeEach
-  void setup() {
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-    headers.setBasicAuth("bgp-client-name", "bgp-client-pw");
-
-    // Mock autowired dependencies
-    RestTemplateBuilder restTemplateBuilderMock = Mockito.mock(RestTemplateBuilder.class);
-    RestTemplate restTemplateMock = Mockito.mock(RestTemplate.class);
-    when(restTemplateBuilderMock.build()).thenReturn(restTemplateMock);
-    UrlUtils urlUtilsMock = Mockito.mock(UrlUtils.class);
-    when(urlUtilsMock.createLobbyServiceUri(anyString(), anyString()))
-        .thenReturn(createMockedUri());
-
-    for (int i = 0; i < usernames.length; i++) {
-      when(restTemplateMock.postForEntity(
-          urlUtilsMock.createLobbyServiceUri(
-              "/oauth/login",
-              "grant_type=password&username=" + usernames[i] + "&password=" + passwords[i]
-          ),
-          new HttpEntity<>(null, headers),
-          TokensInfo.class
-      )).thenReturn(authService.login(usernames[i], passwords[i]));
-    }
-
-    for (String token : tokens) {
-      when(restTemplateMock.postForEntity(
-          urlUtilsMock.createLobbyServiceUri(
-              "/oauth/login",
-              "grant_type=refresh_token&refresh_token=" + token
-          ),
-          new HttpEntity<>(null, headers),
-          TokensInfo.class
-      )).thenReturn(authService.login(token));
-
-      when(restTemplateMock.getForEntity(
-          urlUtilsMock.createLobbyServiceUri(
-              "/oauth/username",
-              "access_token=" + token
-          ),
-          TokensInfo.class
-      )).thenReturn(authService.login(token));
-
-      when(restTemplateMock.postForEntity(
-          urlUtilsMock.createLobbyServiceUri(
-              "/oauth/active",
-              "access_token=" + token
-          ),
-          new HttpEntity<>(null, headers),
-          Void.class
-      )).thenReturn(authService.logout(token));
-    }
-  }*/
 
   @Test
   void testValidLogin() {
@@ -165,5 +94,29 @@ public class AuthServiceTest {
   void testInvalidLogout() {
     ResponseEntity<Void> invalidResponse = authService.logout(tokens[1]);
     assertTrue("Invalid logout should return 400", invalidResponse.getStatusCodeValue() == 400);
+  }
+
+  @Test
+  void testValidVerifyPlayer() {
+    assertTrue(
+        "Valid player and session should return true",
+        authService.verifyPlayer(sessionIds[0], tokens[0], null)
+    );
+  }
+
+  @Test
+  void testInvalidVerifyPlayer() {
+    assertFalse(
+        "Invalid session should return false",
+        authService.verifyPlayer(sessionIds[1], tokens[0], null)
+    );
+    assertFalse(
+        "Invalid token should return false",
+        authService.verifyPlayer(sessionIds[0], tokens[1], null)
+    );
+    assertFalse(
+        "Both invalid should return false",
+        authService.verifyPlayer(sessionIds[1], tokens[1], null)
+    );
   }
 }
