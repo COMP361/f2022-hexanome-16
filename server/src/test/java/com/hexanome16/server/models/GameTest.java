34cb98a0a5f1679ed773939e2091757ccb7e5039
UnHappySquid <imad.issafras@mail.mcgill.ca>
2023-01-20 16:28:00 -0500

    Started writting tests for the Game class.

--- server/src/test/java/com/hexanome16/server/models/GameTest.java
+++ server/src/test/java/com/hexanome16/server/models/GameTest.java
@@ -1,18 +1,29 @@
 package com.hexanome16.server.models;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
+
+
 
 /**
  * Tests for {@link Game}.
  */
 public class GameTest {
-  private final Game game = new Game(12345, null, "", "");
+
+  Player imad = new Player("imad", "#FFFFFF");
+  Player tristan =  new Player("tristan", "#FFFFFF");
+  private final Game game = new Game(12345,
+          new Player[]{imad, tristan}, "imad", "");
 
   /**
    * Instantiates a new Game test.
@@ -58,4 +69,56 @@ public class GameTest {
     game.removeOnBoardCard((LevelCard) card);
     assertFalse(game.getOnBoardDeck(Level.ONE).getCardList().contains(card));
   }
+
+  /**
+   * Testing incGameBank with Purchase Map.
+   */
+  @Test
+  public void testIncGameBank() {
+    assertEquals( game.getGameBank(), new GameBank());
+    HashMap<Gem, Integer> myMap = new HashMap<Gem, Integer>();
+    myMap.put(Gem.RUBY, -2);
+    game.incGameBank(new PurchaseMap(myMap));
+    GameBank myGameBank = new GameBank();
+    myGameBank.incBank(-2,
+            0, 0,
+            0, 0, 0);
+    assertEquals(game.getGameBank(), myGameBank);
+  }
+
+
+  /**
+   * Testing availableTwoTokenType(). The shenanigans in the equal is because it needs
+   * to be a set in order for the order not to matter. (Note that Gold tokens are part
+   * of the returned list, IDK if that should be the case at all but for now it is what
+   * it is :) )
+   */
+  @Test
+  public void testAvailableTwoTokensType() {
+    ArrayList<Gem> availableGems = game.availableTwoTokensType();
+    assertEquals(Set.copyOf(availableGems), Set.copyOf(new ArrayList<>(List.of(Gem.RUBY, Gem.SAPPHIRE,
+            Gem.DIAMOND, Gem.EMERALD, Gem.ONYX, Gem.GOLD))));
+  }
+
+  /**
+   * Testing allowedTakeTwoOf.
+   */
+  @Test
+  public void testAllowedTakeTwoOf() {
+    assertTrue(game.allowedTakeTwoOf(Gem.RUBY));
+    game.incGameBank(-7, 0,
+            0, 0, 0, 0);
+    assertFalse(game.allowedTakeTwoOf(Gem.RUBY));
+  }
+
+  /**
+   * Testing giveTwoOf(Gem g, Player p).
+   */
+  @Test
+  public void testGiveTwoOf() {
+
+    assertEquals(imad.getBank(), new PlayerBank());
+  }
+
+
 }
