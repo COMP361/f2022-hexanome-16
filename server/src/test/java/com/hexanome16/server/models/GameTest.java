928c9c0af00d89f670f3dd53603e752ca53634af
UnHappySquid <imad.issafras@mail.mcgill.ca>
2023-01-22 09:11:35 -0500

    Done Testing Game.java

--- server/src/test/java/com/hexanome16/server/models/GameTest.java
+++ server/src/test/java/com/hexanome16/server/models/GameTest.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -20,17 +22,20 @@ import org.junit.jupiter.api.Test;
  */
 public class GameTest {
 
-  Player imad = new Player("imad", "#FFFFFF");
-  Player tristan =  new Player("tristan", "#FFFFFF");
-  private final Game game = new Game(12345,
-          new Player[]{imad, tristan}, "imad", "");
+  Player imad;
+  Player tristan;
+  private Game game;
+
 
   /**
-   * Instantiates a new Game test.
-   *
-   * @throws IOException the io exception
+   * Initiates every test.
    */
-  public GameTest() throws IOException {
+  @BeforeEach
+  public void init() throws IOException {
+    game = new Game(12345,
+            new Player[]{imad, tristan}, "imad", "");
+    imad = new Player("imad", "#FFFFFF");
+    tristan = new Player("tristan", "#FFFFFF");
   }
 
   /**
@@ -96,8 +101,14 @@ public class GameTest {
   @Test
   public void testAvailableTwoTokensType() {
     ArrayList<Gem> availableGems = game.availableTwoTokensType();
-    assertEquals(Set.copyOf(availableGems), Set.copyOf(new ArrayList<>(List.of(Gem.RUBY, Gem.SAPPHIRE,
-            Gem.DIAMOND, Gem.EMERALD, Gem.ONYX, Gem.GOLD))));
+    assertEquals(Set.copyOf(availableGems), Set.copyOf(new ArrayList<>(List.of(Gem.RUBY,
+            Gem.SAPPHIRE, Gem.DIAMOND,
+            Gem.EMERALD, Gem.ONYX, Gem.GOLD))));
+    game.incGameBank(-3, -4,
+            0, 0, 0, 0);
+    availableGems = game.availableTwoTokensType();
+    assertEquals(Set.copyOf(availableGems), Set.copyOf(new ArrayList<>(List.of(Gem.RUBY,
+            Gem.SAPPHIRE, Gem.DIAMOND, Gem.ONYX, Gem.GOLD))));
   }
 
   /**
@@ -116,8 +127,61 @@ public class GameTest {
    */
   @Test
   public void testGiveTwoOf() {
+    PlayerBank myBank =  new PlayerBank();
+    assertEquals(imad.getBank(), myBank);
+    myBank.incBank(2, 0,
+            0, 0, 0, 0);
+    game.giveTwoOf(Gem.RUBY, imad);
+    GameBank gameBank = new GameBank();
+    gameBank.incBank(-2, 0, 0, 0, 0, 0);
+    assertEquals(imad.getBank(), myBank);
+    assertEquals(game.getGameBank(), gameBank);
+  }
 
-    assertEquals(imad.getBank(), new PlayerBank());
+
+  /**
+   * Testing availableThreeTokenType(). The shenanigans in the equal is once again because it needs
+   * to be a set in order for the order not to matter.
+   */
+  @Test
+  public void testAvailableThreeTokensType() {
+    ArrayList<Gem> availableGems = game.availableThreeTokensType();
+    assertEquals(Set.copyOf(availableGems), Set.copyOf(new ArrayList<>(List.of(Gem.RUBY,
+            Gem.SAPPHIRE, Gem.DIAMOND,
+            Gem.EMERALD, Gem.ONYX, Gem.GOLD))));
+    game.incGameBank(-3, -4,
+            0, 0, -7, 0);
+    availableGems = game.availableThreeTokensType();
+    assertEquals(Set.copyOf(availableGems), Set.copyOf(new ArrayList<>(List.of(Gem.RUBY,
+            Gem.SAPPHIRE, Gem.DIAMOND,
+            Gem.EMERALD, Gem.GOLD))));
+  }
+
+  /**
+   * Testing allowedTakeThreeOf(Gem g, Gem g2, Gem g3).
+   */
+  @Test
+  public void testAllowedTakeThreeOf() {
+    assertTrue(game.allowedTakeThreeOf(Gem.RUBY, Gem.SAPPHIRE, Gem.DIAMOND));
+    assertFalse(game.allowedTakeThreeOf(Gem.RUBY, Gem.RUBY, Gem.DIAMOND));
+    game.incGameBank(-7, 0, 0, 0, 0, 0);
+    assertFalse(game.allowedTakeThreeOf(Gem.RUBY, Gem.EMERALD, Gem.DIAMOND));
+  }
+
+  /**
+   * Testing giveThreeOf(Gem g1, Gem g2, Gem g3, Player p).
+   */
+  @Test
+  public void testGiveThreeOf() {
+    PlayerBank myBank =  new PlayerBank();
+    assertEquals(imad.getBank(), myBank);
+    myBank.incBank(1, 1,
+            0, 1, 0, 0);
+    game.giveThreeOf(Gem.RUBY, Gem.DIAMOND, Gem.EMERALD, imad);
+    GameBank gameBank = new GameBank();
+    gameBank.incBank(-1, -1, 0, -1, 0, 0);
+    assertEquals(imad.getBank(), myBank);
+    assertEquals(game.getGameBank(), gameBank);
   }
 
 
