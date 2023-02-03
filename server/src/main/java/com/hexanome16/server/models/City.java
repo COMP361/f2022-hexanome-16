b17359078d7a129347af06dc267fb9673b8fe415
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2023-02-02 21:41:59 -0500

    Add city and visitable interface

--- server/src/main/java/com/hexanome16/server/models/City.java
+++ server/src/main/java/com/hexanome16/server/models/City.java
@@ -3,32 +3,33 @@ package com.hexanome16.server.models;
 import lombok.Getter;
 
 /**
- * Noble class.
+ * City class for the Cities expansion.
  */
-public class Noble implements Reservable, Visitable {
-
+public class City implements Visitable {
   @Getter
   private final CardInfo cardInfo;
 
+
   /**
-   * Instantiates a new Noble.
+   * Instantiates a new City.
    *
    * @param id            the id
    * @param prestigePoint the number of prestige points
    * @param texturePath   the texture path
    * @param price         the price
    */
-  public Noble(int id, int prestigePoint, String texturePath, Price price) {
+  public City(int id, int prestigePoint, String texturePath, Price price) {
     cardInfo = new CardInfo(id, prestigePoint, texturePath, price);
   }
 
+  /**
+   * Add this card to the player's inventory.
+   *
+   * @param inventory the inventory
+   * @return true on success
+   */
   @Override
   public boolean addToInventory(Inventory inventory) {
-    return inventory.acquireNoble(this);
-  }
-
-  @Override
-  public boolean reserveCard(Inventory inventory) {
-    return inventory.reserveNoble(this);
+    return false;
   }
 }
---
+++ server/src/main/java/com/hexanome16/server/models/Visitable.java
@@ -0,0 +1,7 @@
+package com.hexanome16.server.models;
+
+/**
+ * Interface to enable card visits to players.
+ */
+public interface Visitable extends InventoryAddable {
+}
