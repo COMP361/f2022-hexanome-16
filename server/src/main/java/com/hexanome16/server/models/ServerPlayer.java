b7b748045834c36447a1340d3988ffb8ca1c866d
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2023-03-06 17:04:24 -0500

    Add and test canBeVisitedBy to ServerPlayer

--- server/src/main/java/com/hexanome16/server/models/ServerPlayer.java
+++ server/src/main/java/com/hexanome16/server/models/ServerPlayer.java
@@ -162,5 +162,15 @@ public class ServerPlayer extends Player {
         CustomResponseFactory.getResponse(CustomHttpResponses.TAKE_LEVEL_TWO));
   }
 
+  /**
+   * Verifies that the player meets the requirements to be visited by said visitable.
+   *
+   * @param visitor the visitor whose requirements need to be met
+   * @return true if player meets requirements
+   */
+  public boolean canBeVisitedBy(Visitable visitor) {
+    return visitor.playerMeetsRequirements(inventory);
+  }
+
   ////////////////////////////////////////////////////////////////////////////////////////////////
 }
