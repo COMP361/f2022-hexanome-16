01b37cfb908967e0f84dab6066846becd27bee61
Constantin Buruiana <constbur@gmail.com>
2023-03-09 00:56:24 -0500

    fixed client javadoc

--- client/src/main/java/com/hexanome16/client/screens/game/players/EntityType.java
+++ client/src/main/java/com/hexanome16/client/screens/game/players/EntityType.java
@@ -1,5 +1,15 @@
 package com.hexanome16.client.screens.game.players;
 
+/**
+ * This class represents some entity types used for spawning player entities.
+ */
 public enum EntityType {
-  PLAYER_TURN, PLAYER
+  /**
+   * Player turn entity type.
+   */
+  PLAYER_TURN,
+  /**
+   * Player entity type.
+   */
+  PLAYER
 }
