98da693c6deb354e944a959b8360adbfac464339
Constantin Buruiana <constbur@gmail.com>
2023-03-23 12:37:13 -0400

    added deserializer

--- server/src/main/java/com/hexanome16/server/models/actions/DiscardTokenAction.java
+++ server/src/main/java/com/hexanome16/server/models/actions/DiscardTokenAction.java
@@ -1,19 +1,25 @@
 package com.hexanome16.server.models.actions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.hexanome16.common.models.price.Gem;
 import com.hexanome16.common.util.CustomHttpResponses;
 import com.hexanome16.server.util.CustomResponseFactory;
 import java.util.Arrays;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
 
 /**
  * Action representing discarding tokens.
  */
+@Data
+@NoArgsConstructor
+@JsonDeserialize(as = DiscardTokenAction.class)
 public class DiscardTokenAction implements Action {
-
+  @Getter
   private String[] gemsToChooseFrom;
   private String gemsToChooseFromAsString;
   @Getter
