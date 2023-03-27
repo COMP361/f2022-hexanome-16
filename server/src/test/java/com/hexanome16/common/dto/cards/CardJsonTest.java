49446935654d721d10cef5ff67c66b9a0c2a0858
Tristan Leclair-Vani <60434271+TristanLeclair@users.noreply.github.com>
2023-03-27 15:48:16 -0400

    make underTest private

--- server/src/test/java/com/hexanome16/common/dto/cards/CardJsonTest.java
+++ server/src/test/java/com/hexanome16/common/dto/cards/CardJsonTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CardJsonTest {
-  CardJson underTest;
+  private CardJson underTest;
 
   @BeforeEach
   void setUp() {
