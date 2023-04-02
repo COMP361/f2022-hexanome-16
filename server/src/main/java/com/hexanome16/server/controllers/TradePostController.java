aab2ad7eb2f96c1460d8e299609611c941c5e79d
Constantin Buruiana <constbur@gmail.com>
2023-04-02 17:21:57 -0400

    javadoc

--- server/src/main/java/com/hexanome16/server/controllers/TradePostController.java
+++ server/src/main/java/com/hexanome16/server/controllers/TradePostController.java
@@ -31,7 +31,6 @@ public class TradePostController {
    * @param sessionId sessionId of the game.
    * @param username  username of the player.
    * @return the trade posts the player has.
-   * @throws JsonProcessingException json exception
    */
   @GetMapping(value = {"/games/{sessionId}/tradePost"})
   public ResponseEntity<String> getPlayerTradePosts(@PathVariable long sessionId,
